3ae2186c34861ba83d329856d0c97dc021c333e2
James Driscoll <thejamesdriscoll@gmail.com>
2021-12-01 12:17:42 -0600

    added randomValues function

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function random(num){
     return Math.floor(Math.random() * num)
 }
 
+(function addListeners(){
+    //prevents page from refreshing when button is pressed
+    genButton.addEventListener('click', e => {
+        e.preventDefault()
+    })
+    //genButton.addEventListener('click', displayEmails)
+
+})()
+
+function displayEmails(x){
+    
+}
+function randomValues(num){
+    let output = ''
+    const values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    for (let i = 0; i < num; i++) {
+        output += values[random(36)]
+    }
+    return output
+}
